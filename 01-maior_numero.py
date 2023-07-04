a7ff524fd9d97e70a7ff6ad6b3790905f54905be
Matheus Gonçalves <69082886+matheusgoncalvess@users.noreply.github.com>
2023-07-04 10:02:49 -0300

    add file

---
+++ 01-maior_numero.py
@@ -0,0 +1,7 @@
+primeiroValor = int(input('Digite um numero: '))
+segundoValor = int(input('Digite outro número: '))
+
+if primeiroValor > segundoValor:
+    print('O numero maior é: ', primeiroValor)
+elif segundoValor > primeiroValor:
+    print('O numero maior é: ', segundoValor)
