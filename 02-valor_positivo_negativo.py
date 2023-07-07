fbcdb4a64e922b234681220ed6fd730831498a67
Matheus Gonçalves <69082886+matheusgoncalvess@users.noreply.github.com>
2023-07-07 08:35:21 -0300

    add files

---
+++ 02-valor_positivo_negativo.py
@@ -0,0 +1,7 @@
+primeiroValor = int(input('Digite um valor: '))
+
+if primeiroValor > 0:
+    print('O valor', primeiroValor, 'é positivo!')
+elif primeiroValor < 0:
+    print('O valor', primeiroValor, 'é negativo!')
+    
---
+++ 03-verificar_M_F.py
@@ -0,0 +1,14 @@
+verificar = input('Digite uma letra: ')
+
+letra = 'M'
+letra2 = 'F'
+letras = False
+
+if verificar == letra:
+    print(letra)
+elif verificar == letra2:
+    print(letra2)
+elif verificar != letras:
+    print('Sexo invalido')
+
+
